17a27e46a8f6229364c418b57793defc23b47785
Егор Тумаков <el.tumakov@gmail.com>
2020-10-05 17:14:36 +0300

    Добавляет маршрутизацию для новых компонентов

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 import MainScreen from "../main-screen/main-screen";
+import LoginScreen from "../login-screen/login-screen";
+import FavoritesScreen from "../favorites-screen/favorites-screen";
+import RoomScreen from "../room-screen/room-screen";
 
 
 const App = (props) => {
   const {rentOffersCount} = props;
 
   return (
-    <MainScreen rentOffersCount={rentOffersCount} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainScreen rentOffersCount={rentOffersCount} />
+        </Route>
+        <Route exact path="/login">
+          <LoginScreen />
+        </Route>
+        <Route exact path="/favorites">
+          <FavoritesScreen />
+        </Route>
+        <Route exact path="/offer/:id">
+          <RoomScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
