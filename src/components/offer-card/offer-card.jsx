2c49c18313db681f47fb43fe31f8fa4b885ca74e
Егор Тумаков <el.tumakov@gmail.com>
2020-10-12 12:05:41 +0300

    Описывает и вставляет пропсы для OfferCard

--- src/components/offer-card/offer-card.jsx
+++ src/components/offer-card/offer-card.jsx
@@ -1,4 +1,5 @@
 import React, {PureComponent} from "react";
+import PropTypes from "prop-types";
 
 class OfferCard extends PureComponent {
   constructor(props) {
@@ -6,20 +7,38 @@ class OfferCard extends PureComponent {
   }
 
   render() {
+    const {onActiveOffer, offer} = this.props;
+    const {
+      photos,
+      title,
+      premium,
+      type,
+      rating,
+      price
+    } = offer;
+
     return (
-      <article className="cities__place-card place-card">
-        <div className="place-card__mark">
-          <span>Premium</span>
-        </div>
+      <article
+        className="cities__place-card place-card"
+        onMouseover={(evt) => {
+          evt.preventDefault();
+          onActiveOffer();
+        }}>
+        {premium ?
+          <div className="place-card__mark">
+            <span>Premium</span>
+          </div>
+          : ``
+        }
         <div className="cities__image-wrapper place-card__image-wrapper">
           <a href="#">
-            <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Place image" />
+            <img className="place-card__image" src={photos[0]} width="260" height="200" alt="Place image" />
           </a>
         </div>
         <div className="place-card__info">
           <div className="place-card__price-wrapper">
             <div className="place-card__price">
-              <b className="place-card__price-value">&euro;120</b>
+              <b className="place-card__price-value">&euro;{price}</b>
               <span className="place-card__price-text">&#47;&nbsp;night</span>
             </div>
             <button className="place-card__bookmark-button button" type="button">
@@ -31,18 +50,30 @@ class OfferCard extends PureComponent {
           </div>
           <div className="place-card__rating rating">
             <div className="place-card__stars rating__stars">
-              <span style={{width: `80%`}}></span>
+              <span style={{width: `80%`}}>{rating}</span>
               <span className="visually-hidden">Rating</span>
             </div>
           </div>
           <h2 className="place-card__name">
-            <a href="#">Beautiful &amp; luxurious apartment at great location</a>
+            <a href="#">{title}</a>
           </h2>
-          <p className="place-card__type">Apartment</p>
+          <p className="place-card__type">{type}</p>
         </div>
       </article>
     );
   }
 }
+
+OfferCard.propTypes = {
+  onActiveOffer: PropTypes.func.isRequired,
+  offer: PropTypes.shape({
+    photos: PropTypes.arrayOf(PropTypes.string).isRequired,
+    title: PropTypes.string.isRequired,
+    premium: PropTypes.bool.isRequired,
+    type: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    price: PropTypes.number.isRequired
+  }).isRequired
+};
 
 export default OfferCard;
