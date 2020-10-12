49c752593a18606beca7f347e1c5928ddcb222be
Егор Тумаков <el.tumakov@gmail.com>
2020-10-12 11:18:47 +0300

    Создает компонент списка предложений

---
+++ src/components/offers-list/offers-list.jsx
@@ -0,0 +1,17 @@
+import React, {PureComponent} from "react";
+
+class OffersList extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="cities__places-list places__list tabs__content">
+
+      </div>
+    );
+  }
+}
+
+export default OffersList;
