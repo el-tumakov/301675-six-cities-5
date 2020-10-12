d9aecd0cf8586caeae22bd1af5b7a5939a920ebc
Егор Тумаков <el.tumakov@gmail.com>
2020-10-12 16:11:57 +0300

    Добавляет моки для отзывов

---
+++ src/mocks/reviews.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    id: 1,
+    avatar: `https://www.fillmurray.com/200/309`,
+    name: `Johny Depp`,
+    rating: 4,
+    date: `April 2019`,
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tempor, leo sit amet eleifend elementum, odio justo finibus erat, a tempor nunc libero et dolor.`,
+    offerId: 1
+  },
+  {
+    id: 2,
+    avatar: `https://www.fillmurray.com/200/310`,
+    name: `Keira Knightley`,
+    rating: 5,
+    date: `May 2019`,
+    description: `Donec ac ligula in turpis iaculis facilisis at et tellus. Aenean interdum nunc at enim sollicitudin, id ullamcorper ligula tristique.`,
+    offerId: 2
+  },
+  {
+    id: 3,
+    avatar: `https://www.fillmurray.com/200/311`,
+    name: `Monica Bellucci`,
+    rating: 4,
+    date: `June 2019`,
+    description: `Phasellus in augue eu ligula blandit lacinia. Aliquam sed dui quis eros tincidunt suscipit.`,
+    offerId: 2
+  },
+  {
+    id: 4,
+    avatar: `https://www.fillmurray.com/200/312`,
+    name: `Keanu Reeves`,
+    rating: 3,
+    date: `July 2019`,
+    description: `Sed eget aliquet turpis. Nulla mattis nisi a eleifend bibendum.`,
+    offerId: 3
+  }
+];
