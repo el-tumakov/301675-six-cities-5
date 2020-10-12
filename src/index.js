27e07a335c312da7c0636d308ed59d75480d0e8a
Егор Тумаков <el.tumakov@gmail.com>
2020-10-12 10:55:44 +0300

    Подключает моки

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app";
+import offers from "./mocks/offers";
 
 const Settings = {
   RENT_OFFERS_COUNT: 312
@@ -9,6 +10,7 @@ const Settings = {
 ReactDOM.render(
     <App
       rentOffersCount={Settings.RENT_OFFERS_COUNT}
+      offers={offers}
     />,
     document.querySelector(`#root`)
 );
