f1fbf25d7a88ceb471fd5d4542c264b87c38c85a
Егор Тумаков <el.tumakov@gmail.com>
2020-10-19 14:58:30 +0300

    Добавляет и настраивает file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,10 @@ module.exports = {
             {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
+                loader: 'file-loader'
             }
         ],
     },
