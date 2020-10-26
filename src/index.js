63f217a7b3001ece1b3bfd4553d89ad0eb8e2f10
Егор Тумаков <el.tumakov@gmail.com>
2020-10-26 15:42:11 +0300

    Инициализирует в index.js новое хранилище
    - создает хранилище с помощью функции createStore из пакета redux
    - оборачивает App в Provider и передает ссылку на созданное хранилище
    через props
    - при создании хранилища передает функцию для подключения React DevTools

--- src/index.js
+++ src/index.js
@@ -4,17 +4,27 @@ import App from "./components/app/app";
 import offers from "./mocks/offers";
 import owners from "./mocks/owners";
 import reviews from "./mocks/reviews";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+import {reducer} from "./store/reducer";
 
 const Settings = {
   RENT_OFFERS_COUNT: 312
 };
 
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+);
+
 ReactDOM.render(
-    <App
-      rentOffersCount={Settings.RENT_OFFERS_COUNT}
-      offers={offers}
-      owners={owners}
-      reviews={reviews}
-    />,
+    <Provider store={store}>
+      <App
+        rentOffersCount={Settings.RENT_OFFERS_COUNT}
+        offers={offers}
+        owners={owners}
+        reviews={reviews}
+      />
+    </Provider>,
     document.querySelector(`#root`)
 );
