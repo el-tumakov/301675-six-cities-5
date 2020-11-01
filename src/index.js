0b3c8c97c040d4453b2f98a8b497f057b9be303e
Егор Тумаков <el.tumakov@gmail.com>
2020-11-01 20:01:47 +0300

    Добавляет получение экземпляра axios в index.js
    - устанавливает и добавляет react-dev-tools

--- src/index.js
+++ src/index.js
@@ -4,13 +4,20 @@ import App from "./components/app/app";
 import offers from "./mocks/offers";
 import owners from "./mocks/owners";
 import reviews from "./mocks/reviews";
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
 import {Provider} from "react-redux";
 import {reducer} from "./store/reducer";
+import thunk from "redux-thunk";
+import {createAPI} from "./services/api";
+import {composeWithDevTools} from "redux-devtools-extension";
+
+const api = createAPI();
 
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+    composeWithDevTools(
+        applyMiddleware(thunk.withExtraArgument(api))
+    )
 );
 
 ReactDOM.render(
