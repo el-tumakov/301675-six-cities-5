76ecc639c4819ef4df0762d3b0fc894db620f730
Егор Тумаков <el.tumakov@gmail.com>
2020-11-02 15:36:08 +0300

    Поправляет отображение фотографий в галерее

--- src/components/room-gallery/room-gallery.jsx
+++ src/components/room-gallery/room-gallery.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+const MAX_PHOTOS = 6;
+
 const RoomGallery = (props) => {
   const {photos} = props;
+
+  const images = [];
+
+  for (let i = 0; i < photos.length; i++) {
+    if (i === MAX_PHOTOS) {
+      break;
+    }
+
+    images.push(photos[i]);
+  }
 
   return (
     <div className="property__gallery-container container">
       <div className="property__gallery">
-        {photos.map((photo, i) => (
+        {images.map((photo, i) => (
           <div key={photo + i} className="property__image-wrapper">
             <img className="property__image" src={photo} alt="Photo studio" />
           </div>
