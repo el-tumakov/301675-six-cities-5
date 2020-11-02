1406bbbb79d778e7b5f9a78acb2f1e61f13e664a
Егор Тумаков <el.tumakov@gmail.com>
2020-11-02 21:11:06 +0300

    Подключает состояние со статусом авторизации в index.js
    - подключает редьюсер user в rootReducer

--- src/store/reducers/root-reducer.js
+++ src/store/reducers/root-reducer.js
@@ -1,13 +1,16 @@
 import {combineReducers} from "redux";
 import {appProcess} from "./app-process/app-process";
 import {appData} from "./app-data/app-data";
+import {user} from "./user/user";
 
 export const NameSpace = {
   DATA: `DATA`,
   PROCESS: `PROCESS`,
+  USER: `USER`
 };
 
 export default combineReducers({
   [NameSpace.DATA]: appData,
   [NameSpace.PROCESS]: appProcess,
+  [NameSpace.USER]: user
 });
