d9c94784f776295dcc7d9933f257d9cf6d582dd1
Егор Тумаков <el.tumakov@gmail.com>
2020-11-04 17:11:49 +0300

    Реализует логику обновления статуса избранного при клике на кнопку

--- src/components/favorite-button/favorite-button.jsx
+++ src/components/favorite-button/favorite-button.jsx
@@ -1,19 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import PropTypes from "prop-types";
 import {useHistory} from "react-router-dom";
 import {connect} from "react-redux";
+import {setFavorite} from "../../store/api-actions";
 import {AuthorizationStatus} from "../../const";
 
 
 const FavoriteButton = (props) => {
-  const {favorite, authorizationStatus} = props;
+  const {id, favorite, authorizationStatus, onButtonClick} = props;
   const history = useHistory();
+  const [isFavorite, changeFavorite] = useState(favorite);
 
   return (
     <button
       className={
         `${
-          favorite
+          isFavorite
             ? `place-card__bookmark-button--active`
             : ``
         } place-card__bookmark-button button`
@@ -21,9 +23,12 @@ const FavoriteButton = (props) => {
       type="button"
       onClick={(evt) => {
         evt.preventDefault();
+
+        changeFavorite(!isFavorite);
+
         return authorizationStatus === AuthorizationStatus.NO_AUTH
           ? history.push(`/login`)
-          : ``;
+          : onButtonClick(id, +!isFavorite);
       }}
     >
       <svg className="place-card__bookmark-icon" width="18" height="19">
@@ -35,13 +40,21 @@ const FavoriteButton = (props) => {
 };
 
 FavoriteButton.propTypes = {
+  id: PropTypes.number.isRequired,
   favorite: PropTypes.bool.isRequired,
-  authorizationStatus: PropTypes.string.isRequired
+  authorizationStatus: PropTypes.string.isRequired,
+  onButtonClick: PropTypes.func.isRequired
 };
 
 const mapStateToProps = ({USER}) => ({
   authorizationStatus: USER.authorizationStatus
 });
 
+const mapDispatchToProps = (dispatch) => ({
+  onButtonClick(id, status) {
+    dispatch(setFavorite(id, status));
+  }
+});
+
 export {FavoriteButton};
-export default connect(mapStateToProps, null)(FavoriteButton);
+export default connect(mapStateToProps, mapDispatchToProps)(FavoriteButton);
--- src/components/offer-card/offer-card.jsx
+++ src/components/offer-card/offer-card.jsx
@@ -59,7 +59,7 @@ const OfferCard = (props) => {
             <b className="place-card__price-value">&euro;{price}</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
-          <FavoriteButton favorite={favorite} />
+          <FavoriteButton id={id} favorite={favorite} />
         </div>
         <div className="place-card__rating rating">
           <div className="place-card__stars rating__stars">
