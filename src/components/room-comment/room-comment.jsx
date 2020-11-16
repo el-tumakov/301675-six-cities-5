59dcee23260086ab4b75d41072a12848002eaec3
Егор Тумаков <el.tumakov@gmail.com>
2020-11-16 16:33:31 +0300

    Переделывает RoomComment в функциональный компонент и использует хуки

--- src/components/room-comment/room-comment.jsx
+++ src/components/room-comment/room-comment.jsx
@@ -1,4 +1,4 @@
-import React, {PureComponent, createRef} from "react";
+import React, {useState, useEffect, useRef} from "react";
 import PropTypes from "prop-types";
 import {connect} from "react-redux";
 import {uploadComment} from "../../store/api-actions";
@@ -16,7 +16,7 @@ const TITLES = [
   `perfect`
 ];
 
-const createRatingFragment = (changeHandler, state) => {
+const createRatingFragment = (changeHandler, isDisabled) => {
   const ratingInputs = [];
 
   for (let i = MAX_RATING; i > 0; i--) {
@@ -29,7 +29,7 @@ const createRatingFragment = (changeHandler, state) => {
             id={`${i}-stars`}
             type="radio"
             onChange={changeHandler}
-            disabled={state.isDisabled}
+            disabled={isDisabled}
           />
           <label htmlFor={`${i}-stars`} className="reviews__rating-label form__rating-label" title={TITLES[i - 1]}>
             <svg className="form__star-image" width="37" height="33">
@@ -43,135 +43,100 @@ const createRatingFragment = (changeHandler, state) => {
   return ratingInputs;
 };
 
-class RoomComment extends PureComponent {
-  constructor(props) {
-    super(props);
+const RoomComment = (props) => {
+  const {onSubmit, offerId} = props;
 
-    this.state = {
-      comment: ``,
-      rating: ``,
-      isDisabled: false
-    };
+  const [comment, setComment] = useState(``);
+  const [rating, setRating] = useState(``);
+  const [isDisabled, setDisabled] = useState(false);
 
-    this.form = createRef();
-    this.input = createRef();
-    this.submitButton = createRef();
+  const form = useRef();
+  const input = useRef();
+  const submitButton = useRef();
 
-    this.handleCommentChange = this.handleCommentChange.bind(this);
-    this.handleRatingChange = this.handleRatingChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleLoad = this.handleLoad.bind(this);
-    this.handleError = this.handleError.bind(this);
-  }
+  useEffect(() => {
+    if (rating && comment.length >= CommentLength.MIN && !isDisabled) {
+      submitButton.current.disabled = false;
+    } else {
+      submitButton.current.disabled = true;
+    }
+  });
 
-  handleCommentChange(evt) {
+  const handleSubmit = (evt) => {
     evt.preventDefault();
 
-    this.setState({
-      comment: evt.target.value
-    });
-  }
-
-  handleRatingChange(evt) {
-    this.setState({
-      rating: evt.target.value
-    });
-  }
-
-  handleSubmit(evt) {
-    evt.preventDefault();
-
-    const {onSubmit, offerId} = this.props;
-
-    this.disableForm();
+    setDisabled(true);
 
     onSubmit(
         {
-          comment: this.state.comment,
-          rating: this.state.rating
+          comment,
+          rating
         },
         offerId,
-        this.handleLoad,
-        this.handleError
+        handleLoad,
+        handleError
     );
-  }
+  };
 
-  handleLoad() {
-    this.form.current.reset();
-    this.form.current.style.border = null;
+  const handleLoad = () => {
+    form.current.reset();
+    form.current.style.border = null;
 
-    this.setState({
-      comment: ``,
-      rating: ``,
-      isDisabled: false
-    });
-  }
+    setComment(``);
+    setRating(``);
+    setDisabled(false);
+  };
 
-  handleError() {
-    this.form.current.style.border = `1px solid red`;
+  const handleError = () => {
+    form.current.style.border = `1px solid red`;
 
-    this.setState({
-      isDisabled: false
-    });
-  }
+    setDisabled(false);
+  };
 
-  disableForm() {
-    this.submitButton.current.disabled = true;
+  return (
+    <form
+      className="reviews__form form"
+      onSubmit={handleSubmit}
+      ref={form}
+    >
+      <label className="reviews__label form__label" htmlFor="review">Your review</label>
+      <div className="reviews__rating-form form__rating">
+        {createRatingFragment((evt) => {
+          setRating(evt.target.value);
+        }, isDisabled)}
+      </div>
+      <textarea
+        className="reviews__textarea form__textarea"
+        id="review"
+        name="review"
+        placeholder="Tell how was your stay, what you like and what can be improved"
+        value={comment}
+        onChange={(evt) => {
+          evt.preventDefault();
 
-    this.setState({
-      isDisabled: true
-    });
-  }
-
-  render() {
-    return (
-      <form
-        className="reviews__form form"
-        onSubmit={this.handleSubmit}
-        ref={this.form}
+          setComment(evt.target.value);
+        }}
+        ref={input}
+        maxLength={CommentLength.MAX}
+        disabled={isDisabled}
       >
-        <label className="reviews__label form__label" htmlFor="review">Your review</label>
-        <div className="reviews__rating-form form__rating">
-          {createRatingFragment(this.handleRatingChange, this.state)}
-        </div>
-        <textarea
-          className="reviews__textarea form__textarea"
-          id="review"
-          name="review"
-          placeholder="Tell how was your stay, what you like and what can be improved"
-          value={this.state.comment}
-          onChange={this.handleCommentChange}
-          ref={this.input}
-          maxLength={CommentLength.MAX}
-          disabled={this.state.isDisabled}
+      </textarea>
+      <div className="reviews__button-wrapper">
+        <p className="reviews__help">
+          To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
+        </p>
+        <button
+          className="reviews__submit form__submit button"
+          type="submit"
+          disabled
+          ref={submitButton}
         >
-        </textarea>
-        <div className="reviews__button-wrapper">
-          <p className="reviews__help">
-            To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
-          </p>
-          <button
-            className="reviews__submit form__submit button"
-            type="submit"
-            disabled
-            ref={this.submitButton}
-          >
-            Submit
-          </button>
-        </div>
-      </form>
-    );
-  }
-
-  componentDidUpdate() {
-    if (this.state.rating
-      && this.state.comment.length >= CommentLength.MIN
-      && !this.state.isDisabled
-    ) {
-      this.submitButton.current.disabled = false;
-    }
-  }
-}
+          Submit
+        </button>
+      </div>
+    </form>
+  );
+};
 
 RoomComment.propTypes = {
   onSubmit: PropTypes.func.isRequired,
