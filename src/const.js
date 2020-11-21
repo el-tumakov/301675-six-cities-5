05761289087d499d57d14ceae8958eefffeb69f3
Егор Тумаков <el.tumakov@gmail.com>
2020-11-21 12:19:29 +0300

    Удаляет неиспользующиеся константы

--- src/const.js
+++ src/const.js
@@ -1,23 +1,3 @@
-export const FEATURES = [
-  `Wi-Fi`,
-  `Washing machine`,
-  `Towels`,
-  `Heating`,
-  `Coffee machine`,
-  `Baby seat`,
-  `Kitchen`,
-  `Dishwasher`,
-  `Cabel TV`,
-  `Fridge`
-];
-
-export const OFFERS_TYPE = [
-  `apartment`,
-  `room`,
-  `house`,
-  `hotel`
-];
-
 export const CITIES = [
   `Paris`,
   `Cologne`,
@@ -26,15 +6,6 @@ export const CITIES = [
   `Hamburg`,
   `Dusseldorf`,
 ];
-
-export const Coordinates = {
-  PARIS: [48.8566969, 2.3514616],
-  COLOGNE: [50.938361, 6.959974],
-  BRUSSELS: [50.8465573, 4.351697],
-  AMSTERDAM: [52.3727598, 4.8936041],
-  HAMBURG: [53.550341, 10.000654],
-  DUSSELDORF: [51.2254018, 6.7763137]
-};
 
 export const SortTypes = {
   POPULAR: `popular`,
